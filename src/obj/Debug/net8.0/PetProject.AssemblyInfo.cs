c30eff48108910da5dec1a29bbd73f7727734768
CORP\63200510 <phamnguyennguyenhai25122002@gmail.com>
2024-10-22 19:09:50 -0700

    ok

--- src/obj/Debug/net8.0/PetProject.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/PetProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cae7755ea8c2429bac561e783057ce9c351523f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23ac93a9ccfb788d884250f45aaf7b3b173835e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
